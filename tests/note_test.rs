2e86b5d762d7f4ba1fa3659873b2b827ad25e28c
Nick Krichevsky <nick@ollien.com>
2025-01-05 23:10:59 -0500

    Add test to validate incrementing of clobber detection

--- tests/note_test.rs
+++ tests/note_test.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::fs::{self, OpenOptions};
 
 use chrono::{DateTime, FixedOffset, TimeZone};
 use quicknotes::NoteConfig;
@@ -109,6 +109,77 @@ fn opening_two_notes_with_the_same_name_prevents_clobbering() {
         quicknotes::make_note(&config, editor, "my cool note".to_string(), &test_time());
 
     let upd_note_path = second_note_result.expect("failed to write note");
+
+    let upd_original_location_contents =
+        fs::read_to_string(&note_path).expect("failed to open note");
+
+    assert_eq!(
+        upd_original_location_contents, original_note_contents,
+        "original note contents changed"
+    );
+
+    let upd_note_contents = fs::read_to_string(&upd_note_path).expect("failed to open note");
+    insta::assert_snapshot!(upd_note_contents);
+}
+
+#[test]
+fn opening_two_notes_with_the_same_name_prevents_clobbering_even_if_collision_exists_on_disk() {
+    let roots = testutil::setup_filesystem();
+
+    let config = NoteConfig {
+        file_extension: "txt".to_string(),
+        root_dir: roots.note_root.path().to_owned(),
+        temp_root_override: Some(roots.temp_root.path().to_owned()),
+    };
+
+    let mut editor = AppendEditor::new();
+
+    editor.note_contents("hello, world!\n".to_string());
+    let note_path =
+        quicknotes::make_note(&config, editor, "my cool note".to_string(), &test_time())
+            .expect("could not write note");
+
+    let original_note_contents = fs::read_to_string(&note_path).expect("failed to open note");
+
+    // precondition for setting up rest of test
+    assert_eq!(
+        note_path
+            .file_name()
+            .map(|s| s.to_str().expect("filename is not valid unicode")),
+        Some("my-cool-note.txt")
+    );
+
+    // Create dummy files for the clobber repair to collide with
+    OpenOptions::new()
+        .create_new(true)
+        .write(true)
+        .open(dbg!(note_path.with_file_name("my-cool-note-1.txt")))
+        .expect("could not create dummy note");
+
+    OpenOptions::new()
+        .create_new(true)
+        .write(true)
+        .open(note_path.with_file_name("my-cool-note-2.txt"))
+        .expect("could not create dummy note");
+
+    OpenOptions::new()
+        .create_new(true)
+        .write(true)
+        .open(note_path.with_file_name("my-cool-note-3.txt"))
+        .expect("could not create dummy note");
+
+    let mut editor = AppendEditor::new();
+    editor.note_contents("oh no\n".to_string());
+    let second_note_result =
+        quicknotes::make_note(&config, editor, "my cool note".to_string(), &test_time());
+
+    let upd_note_path = second_note_result.expect("failed to write note");
+
+    // The new note should not be 1-3
+    assert_eq!(
+        upd_note_path,
+        note_path.with_file_name("my-cool-note-4.txt")
+    );
 
     let upd_original_location_contents =
         fs::read_to_string(&note_path).expect("failed to open note");
